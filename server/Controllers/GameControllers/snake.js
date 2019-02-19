7d184c69a7ff2f4ed8a46acf679f5dc4620822d0
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-02-20 03:37:08 +0530

    feat: implement snake game unit movement method

--- server/Controllers/GameControllers/snake.js
+++ server/Controllers/GameControllers/snake.js
@@ -1,12 +1,117 @@
+const { SNAKE } = require('./gameConstants');
+
 class Snake {
-  constructor(playerId, initialPositionX, initialPositionY,
-    initialDirection, snakeColor, initialSnakeLength) {
+  /**
+   * @param  {string} playerId
+   * @param  {Object} initialProperty initial values for snake body and motion.
+   * @param  {Function} eatFood Function to check that snake's next movement will be eating food.
+   * @param  {Array} rivalBody Rival snake's body coordinates
+   * @param  {number} intervalNumber
+   */
+  constructor(playerId, initialProperty, eatFood, rivalBody, intervalNumber) {
     this.ownerId = playerId;
-    this.head = { x: initialPositionX, y: initialPositionY };
-    this.direction = initialDirection;
-    this.color = snakeColor;
-    this.bodyLength = initialSnakeLength;
+    this.head = { x: initialProperty.position.x, y: initialProperty.position.y };
+    this.direction = initialProperty.direction;
+    this.velocity = initialProperty.velocity;
+    this.color = initialProperty.color;
+    this.length = initialProperty.length;
+    this.eatFood = eatFood;
+    this.rivalBody = rivalBody;
+    this.interval = intervalNumber; // interval variable, if snake collides we will clear this interval
+    this.life = SNAKE.life;
+    this.lost = false;
+    this.xAxisVelocity = 0; // the x axis movement of snake in each interval
+    this.yAxisVelocity = 0; // the y axis movement of snake in each interval
     this.bodyCoordinates = [];
+    this.createInitialSnakeBody(); // populate initial body of snake according to given length.
+  }
+
+  /**
+   * This function generates snake body coordinates from the given starting head coordinates
+   */
+  createInitialSnakeBody() {
+    switch (this.direction) {
+      case 'down': {
+        this.yAxisVelocity = this.velocity;
+        break;
+      }
+      case 'up': {
+        this.yAxisVelocity = -this.veloctiy;
+        break;
+      }
+      case 'left': {
+        this.xAxisVelocity = -this.velocity;
+        break;
+      }
+      case 'right': {
+        this.xAxisVelocity = this.velocity;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+    // populating the snake body coordinates of snake
+    for (let x = 0; x < this.length; x += 1) {
+      this.moveSnakeOneStep();
+    }
+  }
+
+  /**
+   * Moves the snakes one steps according to relative change on x axis or y axis.
+   */
+  moveSnakeOneStep() {
+    this.head.x += this.xAxisVelocity; // snake head's new x coordinate
+    this.head.y += this.yAxisVelocity; // snake head's new y coordinate
+    if (this.eatFood(this.head)) {
+      this.pushCoordinates();
+      this.length += SNAKE.FOOD_lENGTH_REWARD;
+      for (let x = 0; x < SNAKE.FOOD_lENGTH_REWARD; x += 1) {
+        this.head.x += this.xAxisVelocity; // snake head's new x coordinate
+        this.head.y += this.yAxisVelocity;
+        this.pushCoordinates();
+      }
+      this.life += SNAKE.FOOD_LIFE_REWARD; // increasing the life by 5 second on eating food.
+    } else if (this.hasCollidedWithItself()
+      || this.hasCollidedWithRival()) {
+      this.lost = true;
+      // @TODO: emit a game mechanics event for game lost.
+    } else {
+      this.pushCoordinates();
+    }
+  }
+
+  /**
+   * Pushing new coordinates to body coordinates array of snake
+   */
+  pushCoordinates() {
+    this.bodyCoordinates.push({
+      x: this.head.x,
+      y: this.head.y,
+    });
+    this.snakeLengthControl();
+  }
+
+  /**
+   * strips etc length from snake's body that is more than current length of snake
+   */
+  snakeLengthControl() {
+    if (this.bodyCoordinates.length > this.length) {
+      this.bodyCoordinates.shift();
+    }
+  }
+
+  /**
+   * Checks collision of the head with it's body
+   */
+  hasCollidedWithItself() {
+    // @TODO: implement the collision logic with itself.
+    return false;
+  }
+
+  hasCollidedWithRival() {
+    // @TODO: implement the collision logic with rival snake.
+    return false;
   }
 }
 
--- server/__tests__/snake.test.js
+++ server/__tests__/snake.test.js
@@ -1,30 +1,104 @@
+const { SNAKE } = require('../Controllers/GameControllers/gameConstants');
 const { Snake } = require('../Controllers/GameControllers/snake');
 
-describe('Checks snake class object initiation and the methods on a snake object', () => {
+describe('Test Snake Class', () => {
   let snake;
-  const playerId = 1;
-  const initialPositionX = 200;
-  const initialPositionY = 200;
-  const initialDirection = 'down';
-  const snakeColor = 'black';
-  const initialSnakeLength = 30;
-
+  const playerId = 'zarathustra';
+  const initialProperty = {
+    position: { x: 200, y: 200 },
+    direction: 'down',
+    velocity: 2,
+    color: 'black',
+    length: SNAKE.INITIAL_LENGTH,
+  };
+  const eatFood = () => false;
+  const rivalBody = [];
+  const intervalNumber = 2;
   beforeEach(() => {
-    snake = new Snake(playerId, initialPositionX,
-      initialPositionY, initialDirection, snakeColor, initialSnakeLength);
+    snake = new Snake(playerId, initialProperty, eatFood,
+      rivalBody, intervalNumber);
   });
 
   afterEach(() => {
     snake = null;
   });
 
-  it('should create a new snake instance from Snake Class', () => {
-    expect(snake).toBeDefined();
-    expect(snake.ownerId).toEqual(playerId);
-    expect(snake.head.x).toEqual(initialPositionX);
-    expect(snake.head.y).toEqual(initialPositionY);
-    expect(snake.direction).toEqual(initialDirection);
-    expect(snake.color).toEqual(snakeColor);
-    expect(snake.bodyLength).toEqual(initialSnakeLength);
+  describe('Checks object initialization of Snake class', () => {
+    it('should create a new snake instance from Snake Class', () => {
+      expect(snake).toBeDefined();
+      expect(snake.ownerId).toEqual(playerId);
+      expect(snake.head.x).toEqual(initialProperty.position.x);
+      expect(snake.head.y).toEqual(initialProperty.position.y + initialProperty.length * 2);
+      expect(snake.direction).toEqual(initialProperty.direction);
+      expect(snake.velocity).toEqual(initialProperty.velocity);
+      expect(snake.color).toEqual(initialProperty.color);
+      expect(snake.length).toEqual(initialProperty.length);
+      expect(snake.eatFood).toEqual(eatFood);
+      expect(snake.rivalBody).toEqual(rivalBody);
+      expect(snake.interval).toEqual(intervalNumber);
+      expect(snake.xAxisVelocity).toEqual(0);
+      expect(snake.yAxisVelocity).toEqual(initialProperty.velocity);
+    });
+
+    it('should have a snake\'s initial populated body(bodyCoordinate array length) with given initial length', () => {
+      expect(snake.bodyCoordinates.length).toEqual(initialProperty.length);
+    });
+  });
+
+  describe('method moveSnakeOneStep', () => {
+    it('should move the snake one step on the coordinate/matrix system', () => {
+      const headPositionBeforeMove = JSON.parse(JSON.stringify(snake.head));
+      const snakeLength = snake.length;
+      const velocityOnXaxis = 0;
+      const velocityOnYaxis = 8;
+      snake.xAxisVelocity = velocityOnXaxis;
+      snake.yAxisVelocity = velocityOnYaxis;
+      snake.moveSnakeOneStep();
+      const newHeadPosition = { x: headPositionBeforeMove.x + velocityOnXaxis, y: (headPositionBeforeMove.y + velocityOnYaxis) };
+      expect(newHeadPosition).toEqual(snake.head);
+      expect(newHeadPosition).toEqual(snake.bodyCoordinates[snake.length - 1]);
+      expect(snakeLength).toEqual(snake.length);
+      expect(snakeLength).toEqual(snake.bodyCoordinates.length);
+    });
+
+    it('should handle the process of eat food.', () => {
+      snake.eatFood = () => true;
+      const headPositionBeforeMove = JSON.parse(JSON.stringify(snake.head));
+      const snakeLength = snake.length;
+      const lifeLine = snake.life;
+      const velocityOnXaxis = 0;
+      const velocityOnYaxis = 8;
+      snake.xAxisVelocity = velocityOnXaxis;
+      snake.yAxisVelocity = velocityOnYaxis;
+      snake.moveSnakeOneStep();
+      const pointerXOnOneStep = headPositionBeforeMove.x + velocityOnXaxis;
+      const pointerYOnOneStep = headPositionBeforeMove.y + velocityOnYaxis;
+      const changeInYpointerOnFoodReward = 15 * velocityOnYaxis;
+      const newYPointer = pointerYOnOneStep + changeInYpointerOnFoodReward;
+      const newHeadPosition = { x: pointerXOnOneStep, y: newYPointer };
+      expect(newHeadPosition).toEqual(snake.head);
+      expect(newHeadPosition).toEqual(snake.bodyCoordinates[snake.length - 1]);
+      expect(snakeLength + SNAKE.FOOD_lENGTH_REWARD).toEqual(snake.length);
+      expect(snakeLength + SNAKE.FOOD_lENGTH_REWARD).toEqual(snake.bodyCoordinates.length);
+      expect(lifeLine + SNAKE.FOOD_LIFE_REWARD).toEqual(snake.life);
+    });
+
+    it('should handle the process of collision itself.', () => {
+      snake.hasCollidedWithItself = () => true;
+      const oldBodyCoordinates = JSON.parse(JSON.stringify(snake.bodyCoordinates));
+      snake.moveSnakeOneStep();
+      const newBodyCoordinates = snake.bodyCoordinates;
+      expect(snake.lost).toBeTruthy();
+      expect(oldBodyCoordinates).toEqual(newBodyCoordinates);
+    });
+
+    it('should handle the process of collision itself.', () => {
+      snake.hasCollidedWithRival = () => true;
+      const oldBodyCoordinates = JSON.parse(JSON.stringify(snake.bodyCoordinates));
+      snake.moveSnakeOneStep();
+      const newBodyCoordinates = snake.bodyCoordinates;
+      expect(snake.lost).toBeTruthy();
+      expect(oldBodyCoordinates).toEqual(newBodyCoordinates);
+    });
   });
 });
